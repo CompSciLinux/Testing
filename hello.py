2cc766f6fc0ecad7889609b23158e07d76cec8a7
peter inglis <peter.inglis@glasgow.ac.uk>
2021-09-11 23:12:46 +0100

    this is not going to work is it?

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def simple_method():
-    print('this is a useless method!')
+    print('I think this is a useless method! but I have a feeling that someone has already mentioned this fact...')
 
 if __name__ == '__main__':
     simple_method()
